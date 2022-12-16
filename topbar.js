4a82dd214cf5ccc505ccd24b00d740aef5834bbc
Avity <98060621+avityred@users.noreply.github.com>
2022-12-16 22:10:20 +0200

    Add files via upload

---
+++ topbar.js
@@ -0,0 +1,15 @@
+function change() {
+    var x = document.getElementById("Topnav");
+    if (x.className === "topnav") {
+      x.className += ".responsive";
+    } else {
+      x.className = "topnav";
+    }
+} 
+
+function change() {
+  var x = document.getElementById("contacti");
+  if (x.className === "ba") {
+    x.id === ".active"
+  }
+} 
